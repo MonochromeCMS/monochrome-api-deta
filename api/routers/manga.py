539d5e06477ea65fa6d199fc9b11dd64bd7661d1
Carlos Ruiz <d34dplayer@protonmail.com>
2021-10-24 23:07:28 +0200

    Upload user avatar

--- api/routers/manga.py
+++ api/routers/manga.py
@@ -4,7 +4,7 @@ from uuid import UUID
 from os import path
 from tempfile import TemporaryFile
 from typing import Optional, List
-from fastapi import APIRouter, Depends, status, Query, File, UploadFile, BackgroundTasks
+from fastapi import APIRouter, Depends, status, Query, File, UploadFile
 
 from .auth import get_connected_user, auth_responses, Permission, get_active_principals
 from ..fastapi_permissions import has_permission, permission_exception
@@ -149,7 +149,7 @@ put_cover_responses = {
     **get_responses,
     400: {
         "description": "The cover isn't a valid image",
-        **BadRequestHTTPException.open_api("image_name is not an image"),
+        **BadRequestHTTPException.open_api("<image_name> is not an image"),
     },
     200: {
         "description": "The edited manga",
@@ -159,11 +159,11 @@ put_cover_responses = {
 
 
 @router.put("/{manga_id}/cover", responses=put_cover_responses)
-async def set_manga_cover(
-    tasks: BackgroundTasks, payload: UploadFile = File(...), manga: Manga = Permission("edit", _get_manga)
-):
+async def set_manga_cover(payload: UploadFile = File(...), manga: Manga = Permission("edit", _get_manga)):
     if not payload.content_type.startswith("image/"):
         raise BadRequestHTTPException(f"'{payload.filename}' is not an image")
 
-    tasks.add_task(save_cover, manga.id, payload.file)
+    save_cover(manga.id, payload.file)
+    await manga.save()
+
     return manga
