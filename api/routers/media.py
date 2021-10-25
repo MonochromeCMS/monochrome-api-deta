95701956a27e1892f2d1aa0909e6d6555e6691be
Carlos Ruiz <d34dplayer@protonmail.com>
2021-10-25 15:35:21 +0200

    404 code if file not found in Deta Drive

--- api/routers/media.py
+++ api/routers/media.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from fastapi.responses import StreamingResponse
 
 from ..fs import media
+from ..exceptions import NotFoundHTTPException
 
 router = APIRouter(prefix="/media", tags=["Media"])
 
@@ -10,5 +11,9 @@ router = APIRouter(prefix="/media", tags=["Media"])
 async def get_media_file(file: str):
     """Get the media files from Deta Drive"""
     headers = {"Cache-Control": "max-age=1728000"}
-    res = media.get(file)
+
+    try:
+        res = media.get(file)
+    except FileNotFoundError:
+        raise NotFoundHTTPException()
     return StreamingResponse(res.iter_chunks(1024), media_type="image/png", headers=headers)
